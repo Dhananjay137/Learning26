9133cc8880c7da40be5606d14f0566f9525fb57a
Dhananjay137 <dhananjayrathod006@gmail.com>
2026-01-28 22:42:35 +0530

    add header.css, HeaderComponent.jsx ,FooterComponent.jsx ,ContentComponent.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { HeaderComponent } from './components/HeaderComponent'
+import { FooterComponent } from './components/FooterComponent'
+import { ContentComponent } from './components/ContentComponent'
 //import './App.css'
 
 function App() {
-  var year=2026
-  var greet='react project'
-  var isAvailable = true
-
-  var student = {
-    id: 1,
-    name: 'manoj',
-    city: 'Ahmedabad'
-  }
-
   return (
     <>
-    <h1>WELCOME</h1>
-    <h2>{greet}</h2>
-    <h3>{year}</h3>
-    <p>is availabe???{isAvailable == true ? 'TRUE':'FALSE'}</p>
-    <h2>ID: {student.id}</h2>
-    <h2>Name: {student.name}</h2>
-    <h2>city: {student.city}</h2>
+    <HeaderComponent></HeaderComponent>
+    <ContentComponent></ContentComponent>
+    <FooterComponent></FooterComponent>
     </>
   )
 }
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../assets/css/header.css"
+
+export const HeaderComponent = () => {
+  return (
+    <h1 className='header'>HeaderComponent</h1>
+  )
+}
